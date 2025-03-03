032495bbca44f05ee38e24f9705f24f1ad6c658c
blazer-mot <motolandblazer@tutamail.com>
2025-03-03 12:48:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+class oktagon: 
+    def__init__(self,side,corner,k):
+        self.side = side
+        self.corner = 2.3561944902
+        self.k = 1 + sqrt(2)
+        def RO (self)
+            R = sqrt(self.k/self.k-1) * self.side
+        def rO (self)
+            r = self.side
+        
